b0d3ea453bf7ac71969f60281d71f3cb8648c178
Hendrik Schawe <surt91@gmail.com>
2020-05-14 10:38:21 +0200

    documenting the MarkovChain trait

--- src/markovchain.rs
+++ src/markovchain.rs
@@ -1,7 +1,13 @@
 use rand::Rng;
 
 pub trait MarkovChain {
+    /// introduce a small change to propose as the next state in the chain
     fn change(&mut self, rng: &mut impl Rng);
+
+    /// undo the previous `change`
     fn undo(&mut self);
+
+    /// the defining value of the current state
+    /// e.g. for a Metropolis algorithm on this chain, it should return the energy
     fn value(&self) -> f64;
 }
